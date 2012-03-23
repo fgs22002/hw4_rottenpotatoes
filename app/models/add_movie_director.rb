be81142e791d7e0c2242c39392d50f9e96b6a3e2
saasbook <saasbook@saasbook.(none)>
2012-03-23 09:35:59 -0700

    add-movie-director

---
+++ app/models/add_movie_director.rb
@@ -0,0 +1,8 @@
+class AddDirector < ActiveRecord::Migration
+  def up
+     add_column :movies, :director, :string, :default => ""
+  end
+  
+  def down
+     remove_column :movies, :director
+  end
